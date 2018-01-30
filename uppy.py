e6a71e3d013a08418d9c2d01d85c9d675216a933
Bobby Towers <sdfwer124@gmail.com>
2018-01-30 07:16:04 -0800

    I actually made something

---
+++ uppy.py
@@ -0,0 +1,20 @@
+import Tkinter as tk
+from subprocess import call
+
+button_flag = True
+
+def click01():
+    global button_flag
+    call('up')
+
+root = tk.Tk()
+frame1 = tk.Frame(root)
+frame1.pack(side=tk.TOP, fill=tk.X)
+
+photo1 = tk.PhotoImage(file="up.gif")
+button1 = tk.Button(frame1, image=photo1,
+    text="Update System", command=click01)
+button1.pack(side=tk.LEFT, padx=2, pady=2)
+button1.image = photo1
+
+root.mainloop()
